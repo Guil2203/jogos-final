74509958f42925225ac182439aff61786b60ee5b
Gui2203 <guiribmir@gmail.com>
2026-07-07 22:25:24 -0300

    add sounds to button + tween polish

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,22 +1,32 @@
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private bool Pressed = false;
     private bool IsHovering = false;
+    private bool Released = false;
+    [SerializeField] private int StartDelay = 500;
     [SerializeField] private float LerpSpeed = 1;
     [SerializeField] private EventTrigger _playButton;
+    
+    [Header("Audios")]
+    [SerializeField] private AudioSource audioHoverIn, audioHoverOut, audioPointerDown, audioPointerUp;
+    
+    
 
     private void Awake() {
         EventTrigger.Entry entry1 = new EventTrigger.Entry(); // criação de entrada para EventSystem
             entry1.eventID = EventTriggerType.PointerDown; // definição do tipo de entrada como aperto de Click
-            entry1.callback.AddListener((eventData) => StartGame()); // definição de função de entrada como StartGame
+            entry1.callback.AddListener((eventData) => Pressed = true); // definição de função de entrada como StartGame
+            entry1.callback.AddListener((eventData) => audioPointerDown.Play());
 
         EventTrigger.Entry entry2 = new EventTrigger.Entry(); // criação de entrada para EventSystem
             entry2.eventID = EventTriggerType.PointerEnter; // definição do tipo de entrada como o mouse entrando na área
             entry2.callback.AddListener((eventData) => IsHovering = true);
+            entry2.callback.AddListener((eventData) => audioHoverIn.Play());
 
         EventTrigger.Entry entry3 = new EventTrigger.Entry(); // criação de entrada para EventSystem
             entry3.eventID = EventTriggerType.PointerExit; // definição do tipo de entrada como o mouse saindo da área
@@ -26,23 +36,41 @@ public class MenuManager : MonoBehaviour
             entry4.eventID = EventTriggerType.Deselect; // definição do tipo de entrada como solto de Click
             entry4.callback.AddListener((eventData) => IsHovering = false);
 
+        EventTrigger.Entry entry5 = new EventTrigger.Entry(); // criação de entrada para EventSystem
+            entry5.eventID = EventTriggerType.PointerUp; // definição do tipo de entrada como solto de Click
+            entry5.callback.AddListener((eventData) => Released =  true);
+            entry5.callback.AddListener((eventData) => StartGame());
+        
         _playButton.triggers.Add(entry1);
         _playButton.triggers.Add(entry2);
         _playButton.triggers.Add(entry3);
         _playButton.triggers.Add(entry4);
+        _playButton.triggers.Add(entry5);
     }
 
-    private void StartGame()
+    private async void StartGame()
     {
-        
+        await Task.Delay(StartDelay);
         SceneManager.LoadScene(1);
     }
 
     private void Update()
     {
+        if (Released)
+        {
+            _playButton.transform.localScale = Vector3.Lerp(_playButton.transform.localScale, (new Vector3(1,1,1)), (LerpSpeed * 4) * Time.deltaTime);
+            return;
+        }
+        
+        if (Pressed)
+        {
+            _playButton.transform.localScale = Vector3.Lerp(_playButton.transform.localScale, (new Vector3(0.9f, 0.9f, 0.9f)), (LerpSpeed * 4) * Time.deltaTime);
+            return;
+        }
+        
         if (IsHovering)
         {
-            _playButton.transform.localScale = Vector3.Lerp(_playButton.transform.localScale, (new Vector3(1.5f,1.5f,1.5f)), LerpSpeed * Time.deltaTime);
+            _playButton.transform.localScale = Vector3.Lerp(_playButton.transform.localScale, (new Vector3(1.1f,1.1f,1.1f)), LerpSpeed * Time.deltaTime);
         }
         else
         {
